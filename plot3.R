c599ea0f2d9c00b07229e385e6d9086562204ead
Tanveer Ansari <tanveeransari@hotmail.com>
2015-04-07 15:38:47 -0500

    Plot 1,2,3 complete

--- plot3.R
+++ plot3.R
@@ -6,7 +6,15 @@ all<-all[all$Date2=="2007-02-01"|all$Date2=="2007-02-02",]
 all$Date2<-strptime(paste(all$Date, all$Time),"%d/%m/%Y %H:%M:%S")
 #dataTFS<-data[weekdays(data$Date) %in% c("Thursday", "Friday", "Saturday"),]
 #save as 480X480 png
-png("plot2.png",height = 480, width = 480, units="px")
-with(all, plot(Date2, Global_reactive_power, type="n"))
-plot(x=all$Date2, y=all$Global_active_power,type = "l", ylab="Global Active Power (kilowatts)",xlab="")
+png("plot3.png",height = 480, width = 480, units="px")
+# lay out the outline of the chart, y label and y axis limits
+with(all, plot(Date2, Sub_metering_1, type="n",ylab="Energy sub metering", xlab="", ylim=c(0,38)))
+# plot sub meters for each metering type
+with(all, points(Date2, Sub_metering_1, type="l", col="black"))
+with(all, points(Date2, Sub_metering_2, type="l", col="red"))
+with(all, points(Date2, Sub_metering_3, type="l", col="blue"))
+# plot legend at top right
+legend("topright",pch=95, col = c("black","red","blue"), 
+       legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+#save the plot
 dev.off()
