e9bec664bed01752604e18ec963df73313fabdc4
Tanveer Ansari <tanveeransari@hotmail.com>
2015-04-07 14:42:52 -0500

    Plot1and2Done-StartedWorkingon3

---
+++ plot2.R
@@ -0,0 +1,11 @@
+all<-read.csv("household_power_consumption.txt", na.strings="?", header=TRUE,sep = ";", colClasses=c("character","character", "numeric","numeric","numeric","numeric","numeric","numeric","numeric"))
+all$Date2<-strptime(as.Date(all$Date,"%d/%m/%Y"),"%Y-%m-%d")
+#filter for 2007Feb first 2 days
+all<-all[all$Date2=="2007-02-01"|all$Date2=="2007-02-02",]
+#use both date and time for posixlt, not just date
+all$Date2<-strptime(paste(all$Date, all$Time),"%d/%m/%Y %H:%M:%S")
+#dataTFS<-data[weekdays(data$Date) %in% c("Thursday", "Friday", "Saturday"),]
+#save as 480X480 png
+png("plot2.png",height = 480, width = 480, units="px")
+plot(x=all$Date2, y=all$Global_active_power,type = "l", ylab="Global Active Power (kilowatts)",xlab="")
+dev.off()
